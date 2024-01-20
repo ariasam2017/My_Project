ba3bff77091a793a67985e968d36abd8d8d30407
Amirhossein <amir.kingstars@gmail.com>
2024-01-20 20:54:40 +0330

    commit

---
+++ src/main/java/Jalase3/Jalase3.java
@@ -0,0 +1,16 @@
+package Jalase3;
+
+public class Jalase3 {
+    public static void main(String[] args) {
+    avg(10,100);
+    int x=2;
+    x+=5;
+
+        System.out.println(x);
+
+    }
+    public static void avg(Integer a,Integer b){
+        System.out.println((a+b)/2);
+    }
+
+}
