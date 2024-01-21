953f29f2fd71d0e8a1c02397d2ea101ee70c0ea8
Amirhossein <amir.kingstars@gmail.com>
2024-01-21 09:35:16 +0330

    commit

--- src/main/java/Jalase3/Jalase3.java
+++ src/main/java/Jalase3/Jalase3.java
@@ -2,7 +2,7 @@ package Jalase3;
 
 public class Jalase3 {
     public static void main(String[] args) {
-   // avg(10,100);       //Avrage
+   // Avrage(10,100);       //Avrage
     int x=2;                    //Assignment Operators Hands-On
     x+=5;
     x-=3;
@@ -13,7 +13,7 @@ public class Jalase3 {
        System.out.println(v);
 
     }
-    public static void avg(Integer a,Integer b){
+    public static void Avrage(Integer a,Integer b){        //Avrage Method
         System.out.println((a+b)/2);
     }
 
