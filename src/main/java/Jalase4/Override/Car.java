21f1520255bdeb8fed097c1472ae9cd4f3219f78
Amirhossein <amir.kingstars@gmail.com>
2024-01-31 11:09:11 +0330

    commit

--- src/main/java/Jalase4/Override/Car.java
+++
@@ -1,9 +0,0 @@
-package Jalase4.Override;
-
-public class Car {
-    private String name;
-    private String color;
-
-
-}
-
--- src/main/java/Jalase4/Override/MainApp.java
+++
@@ -1,7 +0,0 @@
-package Jalase4.Override;
-
-public class MainApp {
-    public static void main(String[] args) {
-        Pride pride=new Pride();
-    }
-}
